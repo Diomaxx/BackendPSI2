8d67ac61a5489582886d9cf7ee3a0a6c87fab7eb
Diomaxx <diogofrancomontenegro06@gmail.com>
2025-06-01 10:26:08 -0400

    ProxyNato

--- seguimiento_donaciones_base/src/main/java/com/psi2/donaciones/config/SecurityConfig.java
+++ seguimiento_donaciones_base/src/main/java/com/psi2/donaciones/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**","/api/**","/images/**","/ws/**").permitAll()
+                        .requestMatchers("/auth/**","/api/**","/images/**","/ws/**","/proxy/**").permitAll()
 
                         .anyRequest().authenticated()
                 )
