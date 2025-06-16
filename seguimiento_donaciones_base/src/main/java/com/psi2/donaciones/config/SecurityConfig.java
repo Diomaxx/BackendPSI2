de079212fc13a35b091436b69541cd8413894b0b
Diomaxx <diogofrancomontenegro06@gmail.com>
2025-06-15 23:46:42 -0400

    Endpoints

--- seguimiento_donaciones_base/src/main/java/com/psi2/donaciones/config/SecurityConfig.java
+++ seguimiento_donaciones_base/src/main/java/com/psi2/donaciones/config/SecurityConfig.java
@@ -38,6 +38,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         return http
+                .cors()
+                .and()
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**","/api/metricas", "/api/donaciones/donantes", "/api/usuarios/**", "/api/solicitudes-sin-responder/crear-completa","/api/solicitudes/apoyo","/images/**","/ws/**","/proxy/**").permitAll()
